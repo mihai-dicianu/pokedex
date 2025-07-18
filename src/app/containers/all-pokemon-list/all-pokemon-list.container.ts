7eb3b55e6fe027829df96ab316d43ef6d9ab4044
mihai-dicianu <mihaidicianu@gmail.com>
2025-07-18 18:43:52 +0300

    Replace hardcoded pokemon list with API call

--- src/app/containers/all-pokemon-list/all-pokemon-list.container.ts
+++ src/app/containers/all-pokemon-list/all-pokemon-list.container.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { SimplePokemon } from 'types/simple-pokemon.type';
 
+import { NamedAPIResource } from 'pokeapi-js-wrapper';
+import { PokemonService } from 'util/pokemon.service';
 import { PokemonListComponent } from '../../components/pokemon-list/pokemon-list.component';
 
 @Component({
@@ -9,95 +10,15 @@ import { PokemonListComponent } from '../../components/pokemon-list/pokemon-list
     template: ` <app-pokemon-list [pokemonList]="allPokemon()" /> `,
 })
 export class AllPokemonListContainer {
-    allPokemon = signal<SimplePokemon[]>(hardcodedResponse.results);
+    allPokemon = signal<NamedAPIResource[]>([]);
+
+    constructor(private readonly pokemonService: PokemonService) {
+        this.pokemonService.getPokemonList(0, 20).then(response => {
+            console.log(response);
+            this.allPokemon.set(response.results);
+
+        });
+    }
 }
 
-// TODO: replace this with a real API call
 // TODO: implement "fetch next page" so we can get more than 20 pokemons
-const hardcodedResponse = {
-    count: 1302,
-    next: 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20',
-    previous: null,
-    results: [
-        {
-            name: 'bulbasaur',
-            url: 'https://pokeapi.co/api/v2/pokemon/1/',
-        },
-        {
-            name: 'ivysaur',
-            url: 'https://pokeapi.co/api/v2/pokemon/2/',
-        },
-        {
-            name: 'venusaur',
-            url: 'https://pokeapi.co/api/v2/pokemon/3/',
-        },
-        {
-            name: 'charmander',
-            url: 'https://pokeapi.co/api/v2/pokemon/4/',
-        },
-        {
-            name: 'charmeleon',
-            url: 'https://pokeapi.co/api/v2/pokemon/5/',
-        },
-        {
-            name: 'charizard',
-            url: 'https://pokeapi.co/api/v2/pokemon/6/',
-        },
-        {
-            name: 'squirtle',
-            url: 'https://pokeapi.co/api/v2/pokemon/7/',
-        },
-        {
-            name: 'wartortle',
-            url: 'https://pokeapi.co/api/v2/pokemon/8/',
-        },
-        {
-            name: 'blastoise',
-            url: 'https://pokeapi.co/api/v2/pokemon/9/',
-        },
-        {
-            name: 'caterpie',
-            url: 'https://pokeapi.co/api/v2/pokemon/10/',
-        },
-        {
-            name: 'metapod',
-            url: 'https://pokeapi.co/api/v2/pokemon/11/',
-        },
-        {
-            name: 'butterfree',
-            url: 'https://pokeapi.co/api/v2/pokemon/12/',
-        },
-        {
-            name: 'weedle',
-            url: 'https://pokeapi.co/api/v2/pokemon/13/',
-        },
-        {
-            name: 'kakuna',
-            url: 'https://pokeapi.co/api/v2/pokemon/14/',
-        },
-        {
-            name: 'beedrill',
-            url: 'https://pokeapi.co/api/v2/pokemon/15/',
-        },
-        {
-            name: 'pidgey',
-            url: 'https://pokeapi.co/api/v2/pokemon/16/',
-        },
-        {
-            name: 'pidgeotto',
-            url: 'https://pokeapi.co/api/v2/pokemon/17/',
-        },
-        {
-            name: 'pidgeot',
-            url: 'https://pokeapi.co/api/v2/pokemon/18/',
-        },
-        {
-            name: 'rattata',
-            url: 'https://pokeapi.co/api/v2/pokemon/19/',
-        },
-        {
-            name: 'raticate',
-            url: 'https://pokeapi.co/api/v2/pokemon/20/',
-        },
-    ],
-};
