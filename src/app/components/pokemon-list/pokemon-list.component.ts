4b3d0edc06bff30867a2e851c8d3c3448f1cd8bc
mihai-dicianu <mihaidicianu@gmail.com>
2025-07-18 19:05:09 +0300

    Migrate part of pokemon list component to tailwind

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,16 +1,17 @@
 import { Component, input } from '@angular/core';
 import { SimplePokemon } from 'types/simple-pokemon.type';
 
+import { injectTwHostClass } from 'util/inject-tw-host-class.util';
 import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item.component';
 
 @Component({
     selector: 'app-pokemon-list',
     template: `
-        <header>
-            <nav>
+        <header class="relative flex flex-row items-start z-2">
+            <nav class="z-2 flex flex-row bg-[#d71f06] py-8 pl-[30px] pr-16 rounded-tl-2xl rounded-br-2xl ">
                 <button class="big-button blue"></button>
             </nav>
-            <div class="nav-shadow"></div>
+            <div class="nav-shadow z-1 absolute left-0 bottom-0  w-6 shadow-[4px_4px_8px_4px_rgba(0,0,0,0.5)]"></div>
 
             <div class="top-bar">
                 <button class="small-button red"></button>
@@ -19,8 +20,8 @@ import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item
             </div>
         </header>
 
-        <div class="section-wrapper">
-            <section>
+        <div class="section-wrapper max-h-[calc(100%-76px)] flex flex-grow p-8 pt-0 -mt-12 bg-[#d71f06] rounded-bl-4xl">
+            <section class="flex flex-col bg-black text-white flex-grow overflow-auto py-12 px-2 rounded-4xl rounded-bl-2xl rounded-tr-none shadow-[inset_0_-1px_2px_3px_#808080]">
                 @for (pokemon of pokemonList(); let index = $index; track pokemon.name) {
                     <app-pokemon-list-item [index]="$index" [pokemon]="pokemon" />
                 }
@@ -34,4 +35,8 @@ import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item
 })
 export class PokemonListComponent {
     readonly pokemonList = input<SimplePokemon[]>([]);
+
+    constructor() {
+        injectTwHostClass(() => 'flex flex-col');
+    }
 }
