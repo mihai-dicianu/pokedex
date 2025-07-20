16ae3e265e8d3f7d71a4c3d3b3bb24ed6a4a4bee
mihai-dicianu <mihaidicianu@gmail.com>
2025-07-20 19:46:25 +0300

    Add tabs functionality to pokemon details

--- src/app/containers/pokemon-detail/pokemon-detail.container.ts
+++ src/app/containers/pokemon-detail/pokemon-detail.container.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PokemonAbilitiesComponent } from 'components/pokemon-abilities/pokemon-abilities.component';
+import { PokemonStatsComponent } from 'components/pokemon-stats/pokemon-stats.component';
 import { Pokedex, Pokemon } from 'pokeapi-js-wrapper';
+import { ClickSoundDirective } from 'util/click-sound.directive';
 import { injectTwHostClass } from 'util/inject-tw-host-class.util';
 import { PokemonInfoComponent } from '../../components/pokemon-info/pokemon-info.component';
 
 @Component({
     selector: 'app-pokemon-detail',
-    imports: [PokemonInfoComponent],
+    imports: [PokemonInfoComponent, CommonModule, PokemonAbilitiesComponent, PokemonStatsComponent, ClickSoundDirective],
     template: `
         <div class="w-full bg-black text-white h-60 p-2 rounded-md shadow-inner">
             @if (pokemon(); as pokemonInfo) {
@@ -21,22 +25,32 @@ import { PokemonInfoComponent } from '../../components/pokemon-info/pokemon-info
             - Be creative, do something you like
         -->
         <div class="flex flex-row *:flex-auto gap-2">
-            <!-- Q: Should these become a separate button/tab component? Maybe use existing lib? -->
-            <button class="bg-cyan-300 p-2 rounded-md">Tab 1</button>
-            <button class="bg-cyan-300 p-2 rounded-md">Tab 2</button>
-            <button class="bg-cyan-300 p-2 rounded-md">Tab 3</button>
+            <button clickSound class="bg-cyan-300 p-2 rounded-md cursor-pointer" [class.bg-cyan-500]="tabNumber() === 0" (click)="tabNumber.set(0)">Abilities</button>
+            <button clickSound class="bg-cyan-300 p-2 rounded-md cursor-pointer" [class.bg-cyan-500]="tabNumber() === 1" (click)="tabNumber.set(1)">Stats</button>
+            <button clickSound class="bg-cyan-300 p-2 rounded-md cursor-pointer" [class.bg-cyan-500]="tabNumber() === 2" (click)="tabNumber.set(2)">Moves</button>
         </div>
 
-        <div class="grow bg-gray-200 p-2 rounded-md">Tab content goes here</div>
+        <div class="grow overflow-y-auto bg-black text-white p-2 rounded-md space-y-4">
+            @if (pokemon(); as pokemonInfo) {
+                @if (tabNumber() === 0) {
+                    <app-pokemon-abilities [pokemon]="pokemonInfo" />
+                } @else if (tabNumber() === 1) {
+                    <app-pokemon-stats [pokemon]="pokemonInfo" />
+                } @else if (tabNumber() === 2) {
+                    <!-- <app-pokemon-moves [pokemon]="pokemonInfo" /> -->
+                }
+            }
+        </div>
     `,
 })
 export class PokemonDetailContainer implements OnInit{
     private readonly route = inject(ActivatedRoute);
     readonly pokemon = signal<Pokemon | undefined>(undefined);
     readonly pokedex = new Pokedex();
+    readonly tabNumber = signal(0);
 
     constructor() {
-        injectTwHostClass(() => 'flex flex-col gap-4 p-5 pt-20');
+        injectTwHostClass(() => 'flex flex-col gap-4 p-5 pt-20 max-w-full overflow-hidden');
     }
 
     ngOnInit(): void {
